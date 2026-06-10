71810dde52c2f2aa82de68ef4bf0d107e78406d4
Bayan Mahmoodi <schmeedainc@gmail.com>
2026-06-10 19:16:42 +0900

    completed basic UI buttons and scene tracking singleton autoloader
    implemented Quit, Settings, and Continue. using this singleton structure we should be able to create dynamic shelled menus. for example opening the settings menu in the main menu and returning should send you back to the main menu while opening that same settings menu in-game will instead return you to the game (and not the main menu

---
+++ scenes/Return.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class Return : Button
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public void OnPressedReturn(){
+		var global = GetNode<Global>("/root/Global");
+		global.GotoScene("res://scenes/main_menu.tscn");
+	}
+}

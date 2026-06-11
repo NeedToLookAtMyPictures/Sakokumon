92dde7017b4a7ed9613c2e90fd44006dab8ff816
Bayan Mahmoodi <schmeedainc@gmail.com>
2026-06-11 14:45:53 +0900

    staged scripts for all main menu and settings buttons/sliders
    currently many of them are hollow awaiting new scenes

--- scripts/UI/GraphicsControl.cs
+++ scripts/UI/GraphicsControl.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Settings : Button
+public partial class GraphicsControl : OptionButton
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -11,12 +11,5 @@ public partial class Settings : Button
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	}
-
-	// Called when the Settings button is pressed.
-	public void OnPressedSettings()
-	{
-		var global = GetNode<Global>("/root/Global");
-		global.GoToScene("res://scenes/interface/settings.tscn");
 	}
 }

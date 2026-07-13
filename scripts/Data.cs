7f972c2b32bd0b0d0cc98fe21cf09017d9b7783a
Jossaya Camille <jossayacamille@gmail.com>
2026-07-13 14:42:08 +0900

    added type field

--- scripts/Data.cs
+++ scripts/Data.cs
@@ -60,6 +60,7 @@ namespace Data
 		public int exitYear {get; set;} // when will the item leave the game?
 		public int legalStartYear {get; set;} // -1 = never legal
 		public int legalEndYear {get; set;}
+		public string type;
 
 		// DO NOT DEFINE IN JSON
 		private List<string> textures;

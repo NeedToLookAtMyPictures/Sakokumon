b61e9bd2b8bd3a241f28a68865b88fecf0b4b3f6
Jossaya Camille <jossayacamille@gmail.com>
2026-07-15 15:53:16 +0900

    Legal/illegal item getters

--- scripts/Data.cs
+++ scripts/Data.cs
@@ -129,11 +129,46 @@ namespace Data
 
 	}
 
-	public struct Level
+	public class Level
 	{
 		public Person[] people {get; set;}
-		public Stats stats {get; set;}
+
+		public Stats stats;
         public bool custom {get; set;}
+
+		private Item[] cig; // for retaining items
+		private Item[] cis; // for existing items being reviewed
+
+		public Item[] currentItemGrid
+		{
+			get
+			{
+				if (cig == null) return [];
+				return cig;
+			}
+			set
+			{
+				if (value == null) return;
+				cig = value;
+			}
+		}
+		public Item[] currentItemStorage
+		{
+			get
+			{
+				if (cis == null) return [];
+				return cis;
+			}
+			set
+			{
+				if (value == null) return;
+				cis = value;
+			}
+		}
+
+		public Level() {}
+
+
 	}
 
 	public struct Stats
@@ -298,6 +333,17 @@ namespace Data
             this.save();
 
         }
+
+		public Item[] LegalItems(int currentYear)
+		{
+			return items.Values.Where(x => currentYear > x.introYear && currentYear < x.exitYear  && (currentYear > x.legalStartYear || currentYear < x.legalEndYear)).ToArray();
+		}
+
+		public Item[] IllegalItems(int currentYear)
+		{
+			return items.Values.Where(x => currentYear > x.introYear && currentYear < x.exitYear  && (currentYear < x.legalStartYear || currentYear > x.legalEndYear)).ToArray();
+		}
+
 
 		public void save()
 		{

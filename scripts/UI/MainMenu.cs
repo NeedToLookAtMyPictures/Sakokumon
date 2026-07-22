133b2c508da0a6f7bcb877a3cb8bab81e494170d
Jossaya Camille <jossayacamille@gmail.com>
2026-07-23 00:31:57 +0900

    UI button implementation across screens

--- scripts/UI/MainMenu.cs
+++ scripts/UI/MainMenu.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Godot;
 
 public partial class MainMenu : Control
@@ -6,9 +7,34 @@ public partial class MainMenu : Control
 	AnimatedSprite2D SideBanner;
 	public override void _Ready()
 	{
+
 		ButtonControl = GetNode<Control>("ButtonControl");
 		SideBanner = GetNode<AnimatedSprite2D>("SideBanner");
+		foreach (Button child in ButtonControl.GetChildren())
+		{
+			Color c = child.Modulate;
+			c.A = 0;
+			child.Modulate = c;
+		}
+		SideBanner.AnimationFinished += _Animate;
+		SideBanner.Frame = 0;
+		SideBanner.Play("default");
+
+	}
+
+	public async void _Animate()
+	{
+		GD.Print("Stopping..");
+		SideBanner.Stop();
+		SideBanner.Frame = 17;
 		
+		foreach (Button child in ButtonControl.GetChildren())
+		{
+			var tween = CreateTween();
+			tween.TweenProperty(child, "modulate:a",1.0f,0.2f)
+			.SetTrans(Tween.TransitionType.Sine)
+			.SetEase(Tween.EaseType.In);
+		}
 
 	}
 
